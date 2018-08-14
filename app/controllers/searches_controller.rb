96d4686426cd4f2eb43d2ef8792ffe40ebcc839e
Justin Sinn <justin.sinn.co@gmail.com>
2018-08-14 23:08:56 +0000

    automatically backed up by learn

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -10,6 +10,6 @@ class SearchesController < ApplicationController
       req.params['near'] = params[:zipcode]
       req.params['query'] = 'coffee shop'
   end
-  render 'search'
+#  render 'search'
 end
 end
